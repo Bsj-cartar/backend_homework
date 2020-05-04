d9a36ee74a41f8cec0f851ed2e018a0f7cbc9505
bsj-cartar <bsj.cartar@gmail.com>
2020-05-04 09:21:11 +0900

    middle-test-1

---
+++ public/middle_test/mid1.js
@@ -0,0 +1,9 @@
+let arr=[];
+let min=1;
+let max=100;
+
+for(let i=0; i < 100; i++) Math.floor(arr[i] = Math.random() * (max - min + 1) + min);
+
+let average= (arr.reduce((a, b) => a+b)) / arr.length;
+
+console.log(average.toFixed(1));
---
+++ public/middle_test/mid2.js
@@ -0,0 +1,11 @@
+let arr=[];
+let min=1;
+let max=100;
+
+for(let i=0; i < 10; i++) Math.floor(arr[i] = Math.random() * (max - min + 1) + min);
+
+console.log(arr);
+
+arr.sort((a,b) => b-a );
+
+console.log(arr);
